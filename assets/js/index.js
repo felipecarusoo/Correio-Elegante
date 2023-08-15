eff648c3bf2e5c3ea64cd1284646512b61883234
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2023-08-15 20:20:53 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-function preencherMensagem(link) {
-    var mensagem = link.getAttribute("data-mensagem");
-    document.getElementById("mensagem").value = mensagem;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".itens-lista-modelos a");
+
+    links.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            const mensagem = link.getAttribute("data-mensagem");
+            document.getElementById("mensagem").value = mensagem;
+        });
+    });
+});
